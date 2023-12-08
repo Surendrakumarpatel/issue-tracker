0322acd546b9d90e5babad1a2a0ad517445778bd
Surendra Kumar <asurendrakumarpatel@gmail.com>
2023-12-08 20:00:02 +0530

    first commit

---
+++ app/Navbar.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='bg-blue-500 p-4 text-white font-bold'>Navbar</div>
+  )
+}
+
+export default Navbar;
---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Navbar from "./Navbar"
+
+ 
+export default function Home() {
+  return (
+     <div>
+      <Navbar/>
+      <h1>Hello</h1>
+     </div>
+  )
+}
