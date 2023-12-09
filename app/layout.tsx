864ba910689dffa06d2f652113b0e71bb310775a
Surendra Kumar <asurendrakumarpatel@gmail.com>
2023-12-09 12:18:32 +0530

    changes theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,9 +3,13 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import Navbar from './Navbar';
-import { Theme } from '@radix-ui/themes';
+import { Theme, ThemePanel } from '@radix-ui/themes';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter(
+  {
+    subsets: ['latin'],
+    variable: "--font-inter"
+  })
 
 export const metadata: Metadata = {
   title: 'Issue Tracker',
@@ -18,9 +22,9 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={inter.variable}>
       <body className={inter.className}>
-        <Theme>
+        <Theme appearance="light" accentColor="purple">
           <Navbar />
           <main className='p-5'>
             {children}
